3918880c0e585f0a3181b640998e4c6e199039d8
Gabriella Hawkes <appsalothelp@gmail.com>
2021-09-22 21:49:42 -0500

    add ExerciseDao

--- app/src/main/java/com/appsalothelpgmail/workout/data/ExerciseDao.java
+++ app/src/main/java/com/appsalothelpgmail/workout/data/ExerciseDao.java
@@ -1,14 +1,28 @@
-package com.appsalothelpgmail.workout;
+package com.appsalothelpgmail.workout.data;
+
+import com.appsalothelpgmail.workout.model.Exercise;
 
 import java.util.List;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 @Dao
 public interface ExerciseDao {
     @Query("SELECT * FROM exercise")
     List<Exercise> loadAllExercises();
+
+    @Insert
+    Exercise addExercise();
+
+    @Update
+    Exercise updateExercise(int id, Exercise exercise);
+
+    @Delete
+    Exercise deleteExercise(int id);
 
 
 
