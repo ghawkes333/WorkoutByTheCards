95a13ed963a4c73777da17bb3ab6395a3b5f5905
Gabriella Hawkes <appsalothelp@gmail.com>
2021-09-22 21:51:14 -0500

    create Card object

--- app/src/main/java/com/appsalothelpgmail/workout/model/Card.java
+++ app/src/main/java/com/appsalothelpgmail/workout/model/Card.java
@@ -1,4 +1,32 @@
 package com.appsalothelpgmail.workout.model;
 
 public class Card {
+    private int mSuit;
+    private String mValue;
+
+    public static final int SUIT_SPADES = 0;
+    public static final int SUIT_CLUBS = 1;
+    public static final int SUIT_HEARTS = 2;
+    public static final int SUIT_DIAMONDS = 3;
+
+    public Card(int suit, String value){
+        mSuit = suit;
+        mValue = value;
+    }
+
+    public int getSuit() {
+        return mSuit;
+    }
+
+    public String getValue() {
+        return mValue;
+    }
+
+    public void setSuit(int suit) {
+        mSuit = suit;
+    }
+
+    public void setValue(String value) {
+        mValue = value;
+    }
 }
